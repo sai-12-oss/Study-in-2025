b3108631775139b5b80019098e21518db37941f5
sai-12-oss <saisandeshk@iisc.ac.in>
2025-01-13 21:32:46 +0530

    Activated Tensorflow

---
+++ Project/AIREX/Tensorflow.py
@@ -0,0 +1,17 @@
+# this is my python file in completing learning tensorflow and Implementing FeedForward Nueral Networks
+
+# To install TensorFlow using conda, follow these steps:
+
+# 1. Create a new conda environment with a compatible Python version:
+# conda create -n tf_env python=3.8
+
+# 2. Activate the new environment:
+# conda activate tf_env
+
+# 3. Install TensorFlow:
+# conda install -c conda-forge tensorflow
+
+# To check if TensorFlow is installed correctly, run the following command in your Python environment:
+# python -c "import tensorflow as tf; print(tf.__version__)"
+
+# ...existing code...
